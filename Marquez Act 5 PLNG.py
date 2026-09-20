589ac53d7a56f15ad71223c2b03129a68c8f65a9
Marquez-Chris <mrchriscarlo8575val@student.fatima.edu.ph>
2026-09-20 15:44:04 +0800

    Marquez Act 5 PLNG.py

--- Marquez Act 5 PLNG.py
+++ Marquez Act 5 PLNG.py
@@ -1,7 +1,18 @@
+def find_last_alphabetical_word():
+ 
+    wr1 = input("Enter a word: ")
+    wr2 = input("Enter a 2nd word: ")
+    wr3 = input("Enter a 3rd word: ")
 
-wr1 = input("Enter a word: ")
-wr2 = input("Enter a 2nd word: ")
-wr3 = input("Enter a 3rd word: ")
 
-words.lower = [wr1,wr2,wr3]
+    words = [wr1.lower(), wr2.lower(), wr3.lower()]
 
+
+    last_word = max(words)
+
+
+    print(f"The word that comes last alphabetically is: {last_word}")
+
+
+
+find_last_alphabetical_word()
